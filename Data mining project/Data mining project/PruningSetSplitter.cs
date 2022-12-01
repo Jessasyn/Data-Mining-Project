239c3c82674c5d8a2f6fe8876cb6729e72e5c5c4
Ivo Melse <ivo@melse.info>
2022-12-01 17:46:28 +0100

    seed

--- Data mining project/Data mining project/PruningSetSplitter.cs
+++ Data mining project/Data mining project/PruningSetSplitter.cs
@@ -1,6 +1,7 @@
 ﻿using SharpLearning.Containers;
 using SharpLearning.Containers.Matrices;
 using SharpLearning.CrossValidation.TrainingTestSplitters;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Data_mining_project
 {
@@ -21,17 +22,19 @@ namespace Data_mining_project
 
         double prunePercentage;
 
-        public PruningSetSplitter(double trainPercentage, double prunePercentage) 
+        int seed;
+
+        public PruningSetSplitter(double trainPercentage, double prunePercentage, int seed=42) 
         {
             this.trainPercentage = trainPercentage;
-            this.prunePercentage = prunePercentage; 
-            
+            this.prunePercentage = prunePercentage;
+            this.seed = seed;
         }
         public PruningSetSplit SplitSet(F64Matrix observations, double[] targets)
         {
             // Use two regular splitters provided by SharpLearning.
-            TrainingTestIndexSplitter<double> trainSplitter = new StratifiedTrainingTestIndexSplitter<double>(trainPercentage);
-            TrainingTestIndexSplitter<double> pruneSplitter = new StratifiedTrainingTestIndexSplitter<double>(prunePercentage / (1 - trainPercentage));
+            TrainingTestIndexSplitter<double> trainSplitter = new StratifiedTrainingTestIndexSplitter<double>(trainPercentage, seed);
+            TrainingTestIndexSplitter<double> pruneSplitter = new StratifiedTrainingTestIndexSplitter<double>(prunePercentage / (1 - trainPercentage), seed);
             // If we use the raw prune percentage, we get the fraction of the data that remains afters splitting off training
 
             TrainingTestSetSplit trainSplit = trainSplitter.SplitSet(observations, targets);
