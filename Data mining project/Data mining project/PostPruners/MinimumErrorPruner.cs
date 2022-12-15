2eb69b5dc0542a66f4e36a63f2f5d969d43871a6
Ivo Melse <ivo@melse.info>
2022-12-15 16:14:30 +0100

    article and min err

---
+++ Data mining project/Data mining project/PostPruners/MinimumErrorPruner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_mining_project.PostPruners
+{
+    public sealed class MinimumErrorPruner : PrunerBase
+    {
+        public override void Prune(IClassifier c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
