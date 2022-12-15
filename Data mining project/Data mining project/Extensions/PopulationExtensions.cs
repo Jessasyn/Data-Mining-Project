a8530418917b09ecafeacf99b680587d125c3222
Jessasyn <alexf.prinsenakker@gmail.com>
2022-12-15 16:36:15 +0100

    Moves populations to extension method

---
+++ Data mining project/Data mining project/Extensions/PopulationExtensions.cs
@@ -0,0 +1,80 @@
+﻿using SharpLearning.Containers.Matrices;
+using SharpLearning.Containers;
+using SharpLearning.DecisionTrees.Nodes;
+
+namespace Data_mining_project.Extensions
+{
+    public static class PopulationExtensions
+    {
+        /// <summary>
+        /// Calculates the populations of each node. <br/>
+        /// The population of a node is defined as the number of observations that pass through a given node.
+        /// We store this in a two-dimensional <see cref="F64Matrix"/>, 
+        /// where the first index is the node index and the second index is the class index.
+        /// </summary>
+        /// <param name="t">The binary tree to calculate the populations for.</param>
+        /// <param name="trainSet">The set of training data, that will be used to obtain the observations.</param>
+        /// <returns>The <see cref="F64Matrix"/> that contains the populations of <paramref name="t"/>.</returns>
+        public static F64Matrix Populations(this BinaryTree t, ObservationTargetSet trainSet)
+        {
+            int rows = t.Nodes.Count;
+            // This is the amount of classes that exist in the data set
+            // Each row in the matrix represents a node and each column is the amount of observations of this class that passed through this node.
+            int cols = t.TargetNames.Length;
+
+            F64Matrix populations = new(rows, cols);
+            Node rootTrainNode = t.Nodes[0];
+            for (int i = 0; i < trainSet.Targets.Length; i++)
+            {
+                // For every row in the training set, we descend the tree and increment the population of the node we end up at.
+                double[] Xi = trainSet.Observations.Row(i);
+                double yi = trainSet.Targets[i];
+                populations.AddXToPopulations(rootTrainNode, Xi, yi, t);
+            }
+
+            return populations;
+        }
+
+        /// <summary>
+        /// Increments the population matrix for the provided node. <br/>
+        /// Then, descends further into the tree, depending on the feature of the node and the value of the observation. <br/>
+        /// This method is based on <see cref="BinaryTree"/>, which can also be found at
+        /// <seealso href=" https://github.com/mdabros/SharpLearning/blob/3f6063fad8886b09c5715c8713541045be7560b9/src/SharpLearning.DecisionTrees/Nodes/BinaryTree.cs"/>.
+        /// </summary>
+        /// <param name="node">The <see cref="Node"/> to start at.</param>
+        /// <param name="observation">The observation values to use.</param>
+        /// <param name="populations">The <see cref="F64Matrix"/> to store the results in.</param>
+        /// <param name="t">The <see cref="BinaryTree"/> that <paramref name="node"/> belongs to.</param>
+        /// <param name="target">The target value for the <paramref name="observation"/>.</param>
+        private static void AddXToPopulations(this F64Matrix populations, Node node, double[] observation, double target, BinaryTree t)
+        {
+            // The node is a leaf node, so we don't need to do anything.
+            if (node.FeatureIndex == -1.0)
+            {
+                return;
+            }
+
+            // This node is not an empty leaf, so we must update the populations matrix.
+            // First, we determine the array index that should be incremented.
+            int rowIndex = node.NodeIndex;
+
+            int colIndex = Array.IndexOf(t.TargetNames, target);
+
+            // Having determined this, we now add one to the count of this class to the populations matrix.
+            populations[rowIndex, colIndex] = populations[rowIndex, colIndex] + 1;
+
+            //If this is the case, the tree would choose the left node, so we need to descend into the left node.
+            if (observation[node.FeatureIndex] <= node.Value)
+            {
+                populations.AddXToPopulations(t.Nodes[node.LeftIndex], observation, target, t);
+                return;
+            }
+            //And otherwise, we have to descend into the right node.
+            else
+            {
+                populations.AddXToPopulations(t.Nodes[node.RightIndex], observation, target, t);
+                return;
+            }
+        }
+    }
+}
---
+++ Data mining project/Data mining project/PostPruners/ErrorComplexityPruner.cs
@@ -0,0 +1,23 @@
+﻿using SharpLearning.Containers;
+using SharpLearning.DecisionTrees.Nodes;
+
+namespace Data_mining_project.PostPruners
+{
+    public sealed class ErrorComplexityPruner : PrunerBase
+    {
+        public override void Prune(IClassifier c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double CalculateErrorCost(double[] observations, Node node, ObservationTargetSet testSet)
+        {
+            //R(t) = \frac{no of examples misclassified in node t \cdot no of examples in node t}{no of examples in node t \cdot no of totalexamples}
+            //R(t) = \frac{no of examples misclassified in node t}{no of total examples}
+            int totalExamples = testSet.Targets.Length;
+
+            //TODO: take populations; 
+            return 0d;
+        }
+    }
+}
